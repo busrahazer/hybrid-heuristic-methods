852e67e4812638f7e15d9c0a19cb4a99f54fef61
busra <minebusrahazer@gmail.com>
2025-12-05 11:52:25 +0300

    Chromosome are created.

--- ga-heuristic-method.py
+++ ga-heuristic-method.py
@@ -59,3 +59,56 @@ class PMSProblem:
         base_demand = base_demand * (1 + noise)
 
         return base_demand    
+
+    class GeneticAlgorithm:    
+        """PMS için Genetik Algoritma"""
+
+        def __init__(self, problem, pop_size=100, generations=200, crossover_rate=0.8, mutation_rate=0.2):
+            self.problem = problem
+            self.pop_size = pop_size
+            self.generations = generations
+            self.crossover_rate = crossover_rate
+            self.mutation_rate = mutation_rate
+
+            # İstatistikler
+            self.best_fitness_history = []
+            self.avg_fitness_history = []
+            self.computation_times = []
+
+        def create_chromosome(self):
+            """
+            Kromozom = 52 haftalık bakım çizelgesi
+            Her gen = o hafta bakıma alınan ekipman ID'si (veya 0 = boş)
+            """
+            chromosome = np.zeros(self.problem.T, dtype=int)
+            equipment_scheduled = set()
+
+            # Her ekipman için bir başlangıç bakımı atama
+            for eq_id in range(1, self.problem.total_equipment + 1):
+                # Rastegele bir başlangıç haftası seç
+                eq_type = self.get_equipment_type(eq_id)
+                duration = self.problem.maintanance_durations[eq_type]
+
+                # Geçerli bir hafta bul
+                max_attempts = 100
+                for i in range(max_attempts):
+                    start_week = np.random.randint(0, self.problem.T - duration + 1)
+
+                    # Bu haftalarda başka ekipman var mı?
+                    if all(chromosome[start_week + i] == 0 for i in range (duration)):
+                        # Bakımı ata
+                        for i in range(duration):
+                            chromosome[start_week + i] = eq_id
+                        equipment_scheduled.add(eq_id)
+                        break  
+
+            return chromosome 
+
+        def get_equipment_type(self, eq_id):
+            """Ekipman ID'sine göre tip belirle"""
+            if eq_id <= self.problem.n_boilers:
+                return 'boiler'
+            elif eq_id <= self.problem.n_boilers + self.problem.n_turbines:
+                return 'turbine'
+            else:
+                return 'distiller'           
